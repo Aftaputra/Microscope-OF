27bd45fd8e6ca28fe4a333095bef63fa98a9d16f
Joel Collins <joel@jtcollins.net>
2018-11-21 12:26:19 +0000

    Fixed error in handling payload parameters

--- openflexure_microscope/api/v1.py
+++ openflexure_microscope/api/v1.py
@@ -353,15 +353,16 @@ class CaptureListAPI(MicroscopeView):
         state = parse_payload(request)
         logging.info(state)
 
-        filename = str(get_from_payload(state, 'filename', default=None))
+        filename = get_from_payload(state, 'filename', default=None)
         keep_on_disk = bool(get_from_payload(state, 'keep_on_disk', default=True))
         use_video_port = bool(get_from_payload(state, 'use_video_port', default=False))
 
-        size_arg = get_from_payload(state, 'size', default=None)
-        if 'width' in size_arg and 'height' in size_arg:
-            resize = (int(size_arg['width']), int(size_arg['height']))
-        else:
-            abort(400)
+        resize = get_from_payload(state, 'size', default=None)
+        if resize:
+            if ('width' in resize) and ('height' in resize):
+                resize = (int(resize['width']), int(resize['height']))  # Convert dict to tuple
+            else:
+                abort(400)
 
         capture_obj = self.microscope.camera.capture(
             write_to_file=True,  # Always write data to disk when using API
