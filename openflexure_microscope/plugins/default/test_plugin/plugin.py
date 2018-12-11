ac5f26ef6ba4279ac63b1c8f585f596be70eddf7
Joel Collins <joel@jtcollins.net>
2018-12-11 17:27:44 +0000

    Removed pointless second plugin from one file

--- openflexure_microscope/plugins/default/test_plugin/plugin.py
+++ openflexure_microscope/plugins/default/test_plugin/plugin.py
@@ -14,18 +14,6 @@ class FirstPlugin(MicroscopePlugin):
         return response
 
 
-class OtherPlugin(MicroscopePlugin):
-    """
-    An example of a second Microscope plugin, loaded from the same plugin file.
-    """
-    def do_something(self):
-        """
-        Demonstrate access to Microscope.camera, and Microscope.stage
-        """
-        return "Stage plugin parent stage: {}".format(self.microscope.stage)
-
-
 PLUGINS = {
     'test1': FirstPlugin,
-    'test2': OtherPlugin,
 }  #: dict: Dictionary describing the plugins. Keys are the names of the plugin's namescape, with a value corresponding to the class of that plugin.
