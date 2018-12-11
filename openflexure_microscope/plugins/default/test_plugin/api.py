be084f6f57762b5718809f242111716e68df9766
Joel Collins <joel@jtcollins.net>
2018-12-11 17:22:01 +0000

    Added API plugin demo

--- openflexure_microscope/plugins/default/test_plugin/api.py
+++ openflexure_microscope/plugins/default/test_plugin/api.py
@@ -1,3 +1,28 @@
+from openflexure_microscope.api.utilities import parse_payload
 from openflexure_microscope.api.v1.views import MicroscopeView
 
-ENDPOINTS = {}
+from flask import request, Response
+
+import logging
+
+
+class PluginTestAPI(MicroscopeView):
+
+    def get(self):
+        data = self.microscope.plugin.test1.run()  # Call a method from our plugin
+        return Response(data)
+
+    def post(self):
+        # Get payload
+        state = parse_payload(request)
+        logging.debug(state)
+
+        # Handle absolute positioning
+        if 'data' in state:  # If value is given in payload JSON
+            data = str(state['data'])  # Process and store that value
+
+        return Response(data)
+
+ENDPOINTS = {
+    'test1': PluginTestAPI
+}
