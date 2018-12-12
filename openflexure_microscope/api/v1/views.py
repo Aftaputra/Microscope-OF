71b5c685ca6a308f1efae62c5190cae9597f0f86
Joel Collins <joel@jtcollins.net>
2018-12-12 15:01:57 +0000

    Created new MicroscopeViewPlugin class to give shortcut to an associated MicroscopePlugin

--- openflexure_microscope/api/v1/views.py
+++ openflexure_microscope/api/v1/views.py
@@ -11,3 +11,16 @@ class MicroscopeView(MethodView):
         self.microscope = microscope
 
         MethodView.__init__(self, **kwargs)
+
+
+class MicroscopeViewPlugin(MicroscopeView):
+
+    def __init__(self, microscope, plugin=None, **kwargs):
+        """
+        Create a generic MethodView with a globally available
+        microscope object passed as an argument, and a plugin
+        reference stored in 'self'. Initially None
+        """
+        self.plugin = plugin
+
+        MicroscopeView.__init__(self, microscope=microscope, **kwargs)
