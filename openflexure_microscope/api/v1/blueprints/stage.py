8367bd7192811530df03eb94eaabf89abed660bb
Joel Collins <joel@jtcollins.net>
2019-01-09 15:58:16 +0000

    Removed redundant imports

--- openflexure_microscope/api/v1/blueprints/stage.py
+++ openflexure_microscope/api/v1/blueprints/stage.py
@@ -1,4 +1,4 @@
-from openflexure_microscope.api.utilities import parse_payload, get_from_payload, gen, get_bool, axes_to_array, JsonResponse
+from openflexure_microscope.api.utilities import gen, axes_to_array, JsonResponse
 from openflexure_microscope.api.v1.views import MicroscopeView
 
 from flask import Response, Blueprint, jsonify, request
