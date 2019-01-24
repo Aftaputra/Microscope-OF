59b634498b56e1694b479bc05249cf0256aa9842
Joel Collins <joel@jtcollins.net>
2019-01-24 14:03:55 +0000

    Moved LockError into exceptions.py

--- openflexure_microscope/exceptions.py
+++ openflexure_microscope/exceptions.py
@@ -1,2 +1,20 @@
+from threading import ThreadError
+
 class TaskDeniedException(Exception):
     pass
+
+class LockError(ThreadError):
+    ERROR_CODES = {
+        'ACQUIRE_ERROR': "Unable to acquire. Lock in use by another thread.",
+        'IN_USE_ERROR': "Lock in use by another thread."
+    }
+
+    def __init__(self, code, lock):
+        self.code = code
+        if code in LockError.ERROR_CODES:
+            self.message = LockError.ERROR_CODES[code]
+        else:
+            self.message = "Unknown error."
+        print("{}: {}".format(self.code, self.message))
+
+        ThreadError.__init__(self)
