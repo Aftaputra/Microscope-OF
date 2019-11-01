d8cfc412e3dc21e7e01d4f66294934615f361a3e
jtc42 <jtc9242@gmail.com>
2019-11-01 18:14:12 +0000

    Added __all__

--- openflexure_microscope/common/tasks/__init__.py
+++ openflexure_microscope/common/tasks/__init__.py
@@ -1,2 +1,14 @@
+__all__ = [
+    "taskify",
+    "tasks",
+    "states",
+    "current_task",
+    "update_task_progress",
+    "cleanup_tasks",
+    "remove_task",
+    "update_task_data",
+    "ThreadTerminationError",
+]
+
 from .pool import tasks, states, current_task, update_task_progress, cleanup_tasks, remove_task, update_task_data, taskify
 from .thread import ThreadTerminationError
