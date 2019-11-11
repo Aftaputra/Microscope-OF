b1a38170a1a1d54c528b283c4bc6eb3438b0348b
Joel Collins <jc2450@bath.ac.uk>
2019-11-11 10:44:57 +0000

    Fixed RecalibrateAPIView POST task

--- openflexure_microscope/plugins/default/camera_calibration/plugin.py
+++ openflexure_microscope/plugins/default/camera_calibration/plugin.py
@@ -15,7 +15,7 @@ from .recalibrate_utils import recalibrate_camera, auto_expose_and_freeze_settin
 class RecalibrateAPIView(MicroscopeViewPlugin):
     def post(self):
         logging.info("Starting microscope recalibration...")
-        task = taskify(self.plugin.recalibrate)
+        task = taskify(self.plugin.recalibrate)()
 
         # Return a handle on the autofocus task
         return jsonify(task.state), 202
