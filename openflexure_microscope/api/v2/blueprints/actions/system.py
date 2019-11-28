af655d20ee49e9e0930ea30ac0759bdf21cb50f5
Joel Collins <joel@jtcollins.net>
2019-11-28 11:50:19 +0000

    Simplified rpi detection

--- openflexure_microscope/api/v2/blueprints/actions/system.py
+++ openflexure_microscope/api/v2/blueprints/actions/system.py
@@ -1,7 +1,7 @@
 from openflexure_microscope.api.views import MicroscopeView
 
 import subprocess
-import io
+import os
 from sys import platform
 
 
@@ -9,41 +9,9 @@ def is_raspberrypi(raise_on_errors=False):
     """
     Checks if Raspberry Pi.
     """
-    try:
-        with io.open('/proc/cpuinfo', 'r') as cpuinfo:
-            found = False
-            for line in cpuinfo:
-                if line.startswith('Hardware'):
-                    found = True
-                    label, value = line.strip().split(':', 1)
-                    value = value.strip()
-                    if value not in (
-                        'BCM2708',
-                        'BCM2709',
-                        'BCM2835',
-                        'BCM2836'
-                    ):
-                        if raise_on_errors:
-                            raise ValueError(
-                                'This system does not appear to be a '
-                                'Raspberry Pi.'
-                            )
-                        else:
-                            return False
-            if not found:
-                if raise_on_errors:
-                    raise ValueError(
-                        'Unable to determine if this system is a Raspberry Pi.'
-                    )
-                else:
-                    return False
-    except IOError:
-        if raise_on_errors:
-            raise ValueError('Unable to open `/proc/cpuinfo`.')
-        else:
-            return False
+    # I mean, if it works, it works...
+    return os.path.exists("/usr/bin/raspi-config")
 
-    return True
 
 class ShutdownAPI(MicroscopeView):
     """
