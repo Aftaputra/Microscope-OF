998d3b4109c480d4e39d20186e1c475913f3cb11
Joel Collins <joel@jtcollins.net>
2019-12-30 18:08:03 +0000

    Added empty class endpoint

--- openflexure_microscope/common/flask_labthings/resource.py
+++ openflexure_microscope/common/flask_labthings/resource.py
@@ -4,6 +4,7 @@ from flask.views import MethodView
 class Resource(MethodView):
     """Currently identical to MethodView
     """
+    endpoint = None
 
     def __init__(self, *args, **kwargs):
         MethodView.__init__(self, *args, **kwargs)
