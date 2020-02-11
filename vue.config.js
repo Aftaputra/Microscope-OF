a815ed549ff3dbfe28c95a1c4af00726f677bd77
Joel Collins <joel@jtcollins.net>
2020-02-11 16:12:09 +0000

    Ran linter

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,26 @@
-var webpack = require('webpack');
+var webpack = require("webpack");
 
 module.exports = {
   configureWebpack: config => {
-    config.target = process.env.VUE_APP_TARGET || 'web',
-    config.entry = (process.env.VUE_APP_TARGET == 'electron-renderer') 
-    ? {
-      main: './src/main.js',
-      app: './src/main.app.js'
-    }
-    :{
-      main: './src/main.js',
-    },
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        'process.env': {
-          PACKAGE: JSON.stringify(require('./package.json'))
-        }
-      })
-    )
+    (config.target = process.env.VUE_APP_TARGET || "web"),
+      (config.entry =
+        process.env.VUE_APP_TARGET == "electron-renderer"
+          ? {
+              main: "./src/main.js",
+              app: "./src/main.app.js"
+            }
+          : {
+              main: "./src/main.js"
+            }),
+      config.plugins.push(
+        new webpack.DefinePlugin({
+          "process.env": {
+            PACKAGE: JSON.stringify(require("./package.json"))
+          }
+        })
+      );
   },
-  outputDir: (process.env.VUE_APP_TARGET == 'electron-renderer') ? './app/dist' : './dist',
-  publicPath: ''
-}
+  outputDir:
+    process.env.VUE_APP_TARGET == "electron-renderer" ? "./app/dist" : "./dist",
+  publicPath: ""
+};
