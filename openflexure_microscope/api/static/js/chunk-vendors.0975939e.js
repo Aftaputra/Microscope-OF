4c4642b6950929d0fea646999a3ab27f988fe714
Joel Collins <joel@jtcollins.net>
2020-02-12 16:41:53 +0000

    Added beta of eV Lite

---
+++ openflexure_microscope/api/static/js/chunk-vendors.0975939e.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9d2f532e5437b2efe083793f70c743ecddd649490bae0e558f3ee462f54beb71
+size 377877
---
+++ openflexure_microscope/api/static/js/main.c89d8483.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9ea3ea783bea1f4aa9438bd3f560ea179773686f330c6a21618eaf5afa7c0616
+size 114840
