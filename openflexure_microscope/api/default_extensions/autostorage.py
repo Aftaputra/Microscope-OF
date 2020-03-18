6b14ea83959d7eab237cc1a9bc4314df061fb3ba
Joel Collins <joel@jtcollins.net>
2020-03-18 17:51:55 +0000

    Moved viewPanel into top-level of extension GUI

--- openflexure_microscope/api/default_extensions/autostorage.py
+++ openflexure_microscope/api/default_extensions/autostorage.py
@@ -241,6 +241,7 @@ def dynamic_form():
     return {
         "icon": "sd_storage",
         "title": "Storage",
+        "viewPanel": "gallery",
         "forms": [
             {
                 "name": "Autostorage",
@@ -248,7 +249,6 @@ def dynamic_form():
                 "isTask": False,
                 "route": "/location-from-title",
                 "emitOnResponse": "globalUpdateCaptures",
-                "viewPanel": "gallery",
                 "submitLabel": "Set path",
                 "schema": [
                     {
