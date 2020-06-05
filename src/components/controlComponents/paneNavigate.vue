599cf0367f8f956b6aae9093ce51e3d1da07f1bb
Joel Collins <joel@jtcollins.net>
2020-06-05 09:59:35 +0100

    Remove component-level accordion animation disable

--- src/components/controlComponents/paneNavigate.vue
+++ src/components/controlComponents/paneNavigate.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="paneNavigate" class="uk-padding-small">
     <div v-if="setPosition">
-      <ul uk-accordion="multiple: true; animation: false">
+      <ul uk-accordion="multiple: true">
         <li>
           <a class="uk-accordion-title" href="#">Configure</a>
           <div class="uk-accordion-content">
