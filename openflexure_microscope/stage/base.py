7c1c49eff2169e0f8fea5b1260bfa8dced270cb4
Joel Collins <j.collins@bath.ac.uk>
2020-06-24 10:39:00 +0100

    Added 5 second timeout to device locks

--- openflexure_microscope/stage/base.py
+++ openflexure_microscope/stage/base.py
@@ -11,7 +11,7 @@ class BaseStage(metaclass=ABCMeta):
     """
 
     def __init__(self):
-        self.lock = StrictLock(name="Stage")
+        self.lock = StrictLock(name="Stage", timeout=5)
 
     @abstractmethod
     def update_settings(self, config: dict):
