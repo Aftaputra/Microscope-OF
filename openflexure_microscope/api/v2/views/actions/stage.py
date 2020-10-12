f30679d4db36505cb8a82c331aff333b9767814a
samuelmcdermott <sjm263@cam.ac.uk>
2020-10-12 16:56:25 +0100

    stage API returns a dict

--- openflexure_microscope/api/v2/views/actions/stage.py
+++ openflexure_microscope/api/v2/views/actions/stage.py
@@ -76,14 +76,12 @@ class StageTypeAPI(ActionView):
         """
         microscope = find_component("org.openflexure.microscope")
         microscope.set_stage(stage_type=args.get("stage_type"))
-        
-        # TODO: Make schema for microscope state
-        return microscope.configuration["stage"]["type"]
+        return {"stage_type": microscope.configuration["stage"]["type"]}
 
     def get(self):
         """
         Get the stage geometry.
         """
         microscope = find_component("org.openflexure.microscope")
-        return microscope.configuration["stage"]["type"]
+        return {"stage_type": microscope.configuration["stage"]["type"]}
 
