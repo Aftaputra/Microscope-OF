4521913f1b0b523dc3783518b8c6f06bc34c936a
Joel Collins <joel@jtcollins.net>
2020-10-13 14:59:59 +0100

    Added OneOf to stage type schema

--- openflexure_microscope/api/v2/views/stage.py
+++ openflexure_microscope/api/v2/views/stage.py
@@ -7,20 +7,24 @@ import logging
 class StageTypeProperty(PropertyView):
     """The type of the stage"""
 
+    schema = fields.String(
+        missing=None,
+        example="SangaStage",
+        OneOf=["SangaStage", "SangaDeltaStage"],
+        description="The translation stage geometry",
+    )
+
     def get(self):
         """
         Get the stage geometry.
         """
         microscope = find_component("org.openflexure.microscope")
-        return microscope.configuration['stage']['type']
-    
-    schema = fields.String(missing=None, example="SangaStage", description="The stage geometry [SangaStage, SangaDeltaStage]")
-    
-    def put(self,stage_type):
+        return microscope.configuration["stage"]["type"]
+
+    def put(self, stage_type):
         """
         Set the stage geometry.
         """
         microscope = find_component("org.openflexure.microscope")
         microscope.set_stage(stage_type=stage_type)
         return microscope.configuration["stage"]["type"]
-
