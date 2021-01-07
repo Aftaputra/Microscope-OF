bd7833c20f4d78f969f3015e36936409d4577c4b
Joel Collins <joel@jtcollins.net>
2021-01-07 10:23:27 +0000

    Test app object initialises

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+from openflexure_microscope.api.app import app, labthing, api_microscope
+from openflexure_microscope.microscope import Microscope
+from openflexure_microscope.camera.base import BaseCamera
+from openflexure_microscope.stage.base import BaseStage
+
+
+def test_app_creation():
+    assert labthing.app is app
+
+
+def test_microscope_creation():
+    assert isinstance(api_microscope, Microscope)
+    assert isinstance(api_microscope.camera, BaseCamera)
+    assert isinstance(api_microscope.stage, BaseStage)
