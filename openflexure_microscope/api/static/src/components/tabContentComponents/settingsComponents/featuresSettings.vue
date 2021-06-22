f5711d953d97da044b17912d0a45912a52d467ba
Richard <richard.bowman@cantab.net>
2021-06-22 12:09:01 +0100

    linter fixes

--- openflexure_microscope/api/static/src/components/tabContentComponents/settingsComponents/featuresSettings.vue
+++ openflexure_microscope/api/static/src/components/tabContentComponents/settingsComponents/featuresSettings.vue
@@ -14,19 +14,24 @@
       for use in clinics acquiring blood smear samples using a 100x
       oil-immersion objective, with a Raspberry Pi Camera v2.
     </p>
-    <p class="uk-margin-small" :class="{'uk-text-muted': !imjoyPermitted}">
+    <p class="uk-margin-small" :class="{ 'uk-text-muted': !imjoyPermitted }">
       <label :disabled="!imjoyPermitted">
-        <input v-model="imjoyEnabled" class="uk-checkbox" type="checkbox" :disabled="!imjoyPermitted"/>
+        <input
+          v-model="imjoyEnabled"
+          class="uk-checkbox"
+          type="checkbox"
+          :disabled="!imjoyPermitted"
+        />
         Enable ImJoy plugin engine
       </label>
     </p>
-    <p class="uk-margin-small" v-if="imjoyPermitted">
+    <p v-if="imjoyPermitted" class="uk-margin-small">
       <a href="https://imjoy.io/">ImJoy</a> enables integration with a wide
       variety of microscopy and image analysis applications, including ImageJ.JS
       and Kaibu. Support for ImJoy within the OFM software is currently
       experimental, and it may slow down loading of the application.
     </p>
-    <p class="uk-margin-small uk-text-muted" v-if="!imjoyPermitted">
+    <p v-if="!imjoyPermitted" class="uk-margin-small uk-text-muted">
       ImJoy plugins are disabled in this build of the OpenFlexure software.
     </p>
     <p class="uk-margin-small">
@@ -76,7 +81,7 @@ export default {
         this.$store.commit("changeGalleryEnabled", value);
       }
     },
-    imjoyPermitted: function () {
+    imjoyPermitted: function() {
       // ImJoy may be disabled using an environment variable.
       // If this function returns false, it is never possible to
       // use ImJoy, so we should gray out the option.
