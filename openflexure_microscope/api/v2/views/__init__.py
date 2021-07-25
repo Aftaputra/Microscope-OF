a8b1efb7cd8163b8372b64ecb9da048945d79166
Richard <richard.bowman@cantab.net>
2021-07-25 01:23:44 +0100

    Removed openapi endpoints (now in LabThings)

--- openflexure_microscope/api/v2/views/__init__.py
+++ openflexure_microscope/api/v2/views/__init__.py
@@ -1,7 +1,6 @@
 from .actions import enabled_root_actions
 from .camera import *
 from .captures import *
-from .docs import *
 from .instrument import *
 from .stage import *
 from .streams import *
