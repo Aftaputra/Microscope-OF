d7d7a8cc8d18549fc1dd874a734b77b85f0ac7ef
Richard <richard.bowman@cantab.net>
2021-08-09 22:01:14 +0100

    Blackened test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -23,19 +23,17 @@ def test_microscope_creation():
 def test_openapi_valid():
     assert validate_spec(labthing.spec)
 
+
 def test_thing_description_valid():
     # TODO: it would be nice to put this into LabThings
     # First load the schema from file and check it validates
-    schema_fname = os.path.join(
-        os.path.dirname(__file__), 
-        "w3c_td_schema.json"
-    )
+    schema_fname = os.path.join(os.path.dirname(__file__), "w3c_td_schema.json")
     schema = json.load(open(schema_fname, "r"))
     jsonschema.Draft7Validator.check_schema(schema)
 
     # Build a TD dictionary
     with labthing.app.test_request_context():
-            td_dict = labthing.thing_description.to_dict()
+        td_dict = labthing.thing_description.to_dict()
 
     # Allow our LabThingsJSONEncoder to encode the RD
     td_json = encode_json(td_dict)
