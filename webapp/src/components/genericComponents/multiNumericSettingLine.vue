79752a2a4cf09ce48c6ff693bdeb247d9402d3c6
Richard Bowman <richard.bowman@cantab.net>
2023-11-02 16:54:22 +0000

    Camera settings for labthings-picamera2
    I've updated URLs and tidied things a bit, so that we can
    now adjust the key camera settings with the new camera
    drivers.

---
+++ webapp/src/components/genericComponents/multiNumericSettingLine.vue
@@ -0,0 +1,121 @@
+<template>
+  <div>
+    <label class="uk-form-label">{{ label }}</label>
+    <div class="input-and-buttons-container">
+      <input
+        v-for="(_v, key) in value"
+        class="uk-form-small numeric-setting-line-input"
+        type="number"
+        v-model="value[key]"
+        :key="key"
+        @focusin="focusIn"
+        @focusout="focusOut"
+        @keydown="keyDown"
+      />
+      <a class="button-next-to-input" @click="readProperty">
+        <i class="material-icons">refresh</i>
+      </a>
+    </div>
+  </div>
+</template>
+  
+<script>
+import axios from "axios";
+
+export default {
+name: "MultiNumericSettingLine",
+
+props: {
+    label: {
+        type: String,
+        required: true
+    },
+    propertyUrl: {
+        type: String,
+        required: true
+    },
+    readBackDelay: {
+        type: Number,
+        default: undefined,
+        required: false
+    },
+},
+
+data: () => {
+    return {
+        value: {},
+        valueOnEnter: undefined
+    }
+},
+
+computed: {
+    readBack: function() {
+        return this.readBackDelay !== undefined;
+    }
+},
+
+mounted() {
+    this.readProperty();
+},
+
+methods: {
+    readProperty: async function() {
+        let response = await axios.get(this.propertyUrl)
+        this.value = response.data
+        console.log("Read property", this.propertyUrl, response.data)
+        return response.data
+    },
+    writeProperty: async function() {
+        try {
+            let requestedValue = Number(this.value)
+            await axios.post(this.propertyUrl, requestedValue)
+            if(this.readBack) {
+                await new Promise(r => setTimeout(r, this.readBackDelay))
+                let newVal = await this.readProperty()
+                if(newVal == requestedValue) {
+                    await this.modalNotify(`Set ${this.label} to ${newVal}.`)
+                } else {
+                    await this.modalNotify(`Set ${this.label} to ${newVal} (requested ${requestedValue}).`)
+                }
+            } else {
+                await this.modalNotify(`Set ${this.label} to ${this.value}.`);
+            }
+        } catch(error) {
+            this.modalError(error); // Let mixin handle error
+        }
+    },
+    focusIn: function(event) {
+        this.valueOnEnter = event.target.value;
+    },
+    focusOut: function(event) {
+        if (this.valueOnEnter != event.target.value) {
+            this.writeProperty(event.target.value);
+        }
+    },
+    keyDown: function(event) {
+        // Pressing enter should set the property, whether or not we think it's changed.
+        if (event.keyCode == 13) {
+            this.writeProperty();
+        }
+    }
+}
+};
+</script>
+
+<style scoped>
+.input-and-buttons-container {
+  display: table;
+  width: 100%;
+}
+.numeric-setting-line-input {
+  display: table-cell;
+  width:100%;
+}
+.button-next-to-input {
+  display: table-cell;
+  padding-left: 20px;
+  padding-right: 5px;
+  vertical-align: middle;
+  cursor: pointer;
+}
+</style>
--- webapp/src/components/tabContentComponents/settingsComponents/cameraSettingsComponents/cameraCalibrationSettings.vue
+++ webapp/src/components/tabContentComponents/settingsComponents/cameraSettingsComponents/cameraCalibrationSettings.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <!--Show auto calibrate if default plugin is enabled-->
-    <div v-if="'recalibrate' in recalibrationLinks" class="uk-margin-small">
+    <div v-if="'full_auto_calibrate' in actions" class="uk-margin-small">
       <taskSubmitter
         :can-terminate="false"
         :requires-confirmation="true"
         :confirmation-message="
           'Start recalibration? This may take a while, and the microscope will be locked during this time.'
         "
-        :submit-url="recalibrationLinks.recalibrate.href"
+        :submit-url="cameraUri + 'full_auto_calibrate'"
         :submit-label="'Full Auto-Calibrate'"
         @response="onRecalibrateResponse"
         @error="modalError"
@@ -16,13 +16,13 @@
       </taskSubmitter>
     </div>
     <div
-      v-if="'auto_exposure_from_raw' in recalibrationLinks"
+      v-if="'auto_expose_from_minimum' in actions"
       class="uk-margin-small"
     >
       <taskSubmitter
         :can-terminate="false"
         :requires-confirmation="false"
-        :submit-url="recalibrationLinks.auto_exposure_from_raw.href"
+        :submit-url="cameraUri + 'auto_expose_from_minimum'"
         :submit-label="'Auto gain &amp; shutter speed'"
         @response="onRecalibrateResponse"
         @error="modalError"
@@ -30,13 +30,13 @@
       </taskSubmitter>
     </div>
     <div
-      v-if="'auto_white_balance_from_raw' in recalibrationLinks"
+      v-if="'calibrate_white_balance' in actions"
       class="uk-margin-small"
     >
       <taskSubmitter
         :can-terminate="false"
         :requires-confirmation="false"
-        :submit-url="recalibrationLinks.auto_white_balance_from_raw.href"
+        :submit-url="cameraUri + 'calibrate_white_balance'"
         :submit-label="'Auto white balance'"
         @response="onRecalibrateResponse"
         @error="modalError"
@@ -44,7 +44,7 @@
       </taskSubmitter>
     </div>
     <div
-      v-if="'auto_lens_shading_table' in recalibrationLinks"
+      v-if="'calibrate_lens_shading' in actions"
       class="uk-margin-small"
     >
       <taskSubmitter
@@ -54,7 +54,7 @@
           'Is the microscope looking at an evenly illuminated, empty field of view? ' +
             'If not, the current image will show through in any images captured afterwards.'
         "
-        :submit-url="recalibrationLinks.auto_lens_shading_table.href"
+        :submit-url="cameraUri + 'calibrate_lens_shading'"
         :submit-label="'Auto flat field correction'"
         @response="onRecalibrateResponse"
         @error="modalError"
@@ -62,23 +62,34 @@
       </taskSubmitter>
     </div>
 
-    <div v-show="showExtraSettings" class="uk-child-width-expand">
-      <button
-        v-if="'flatten_lens_shading_table' in recalibrationLinks"
-        class="uk-button uk-button-danger uk-width-1-1"
-        @click="flattenLensShadingTableRequest"
+    <div 
+      v-show="showExtraSettings"
+      v-if="'flatten_lens_shading_table' in actions"
+      class="uk-child-width-expand"
+    >
+      <taskSubmitter
+        :can-terminate="false"
+        :requires-confirmation="false"
+        :submit-url="cameraUri + 'flat_lens_shading'"
+        :submit-label="'Disable flat field correction'"
+        @response="onRecalibrateResponse"
+        @error="modalError"
       >
-        Disable flat field correction
-      </button>
+      </taskSubmitter>
     </div>
-
-    <div v-if="LstDownloadEnabled">
-      <a
-        class="uk-button uk-button-default uk-width-large uk-margin-small-top uk-align-center"
-        :href="LstDownloadUri"
-        download
-        >Download Lens-Shading Table</a
+    <div 
+      v-show="showExtraSettings"
+      v-if="'reset_lens_shading' in actions"
+      class="uk-child-width-expand"
+    >      <taskSubmitter
+        :can-terminate="false"
+        :requires-confirmation="false"
+        :submit-url="cameraUri + 'reset_lens_shading'"
+        :submit-label="'Reset flat field correction'"
+        @response="onRecalibrateResponse"
+        @error="modalError"
       >
+      </taskSubmitter>
     </div>
   </div>
 </template>
@@ -100,66 +111,35 @@ export default {
       type: Boolean,
       required: false,
       default: true
+    },
+    cameraUri: {
+      type: String,
+      required: true
     }
   },
 
   data: function() {
     return {
-      recalibrationLinks: {},
+      actions: {},
       isCalibrating: false,
       LstDownloadEnabled: false
     };
   },
 
-  computed: {
-    pluginsUri: function() {
-      return `${this.$store.getters.baseUri}/api/v2/extensions`;
-    },
-    LstDownloadUri: function() {
-      return `${this.$store.getters.baseUri}/api/v2/instrument/camera/lst`;
-    }
-  },
-
   mounted() {
-    this.updateRecalibrationLinks();
-    this.checkLstDownload();
+    this.updateActions();
   },
 
   methods: {
-    updateRecalibrationLinks: function() {
-      axios
-        .get(this.pluginsUri) // Get a list of plugins
-        .then(response => {
-          var plugins = response.data;
-          var foundExtension = plugins.find(
-            e => e.title === "org.openflexure.calibration.picamera"
-          );
-          // if AutocalibrationPlugin is enabled
-          if (foundExtension) {
-            // Get plugin action link
-            this.recalibrationLinks = foundExtension.links;
-          } else {
-            this.recalibrationLinks = {};
-          }
-        })
-        .catch(error => {
-          this.modalError(error); // Let mixin handle error
-        });
-    },
-
-    checkLstDownload: function() {
-      axios
-        .get(this.LstDownloadUri) // Get a list of plugins
-        .then(response => {
-          if (response.status === 200) {
-            this.LstDownloadEnabled = true;
-          } else {
-            this.LstDownloadEnabled = false;
-          }
-        })
-        .catch(error => {
-          this.modalError(error); // Let mixin handle error
-        });
+    updateActions: async function() {
+      try{
+          let response = await axios.get(this.cameraUri) // Get the thing description
+          let td = response.data
+          this.actions = td.actions
+          console.log("full auto calibrate in actions", 'full_auto_calibrate' in this.actions)
+      } catch(error) {
+        this.modalError(error) // Let mixin handle error
+      }
     },
 
     onRecalibrateResponse: function() {
