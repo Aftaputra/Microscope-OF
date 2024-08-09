e3e248c46e3071232e13e40c4ed4bcf7ac8294de
Richard Bowman <richard.bowman@cantab.net>
2024-08-09 01:51:11 +0100

    Move server config to a config file
    Currently this is "just" a LabThings config file.

--- src/openflexure_microscope_server/server/__init__.py
+++ src/openflexure_microscope_server/server/__init__.py
@@ -1,43 +1,47 @@
 from __future__ import annotations
 
-from labthings_fastapi.thing_server import ThingServer
-from labthings_sangaboard import SangaboardThing
-from labthings_picamera2.thing import StreamingPiCamera2
+from typing import Optional
 
-from ..things.autofocus import AutofocusThing
-from ..things.camera_stage_mapping import CameraStageMapper
-from ..things.system_control import SystemControlThing
-from ..things.settings_manager import SettingsManager
-from ..things.auto_recentre_stage import RecentringThing
-from ..things.smart_scan import SmartScanThing, BackgroundDetectThing
-from ..things.test import APITestThing
+from labthings_fastapi.server import cli, ThingServer
+import uvicorn
 from .serve_static_files import add_static_files
 from .legacy_api import add_v2_endpoints
 from ..logging import configure_logging, retrieve_log
 
-configure_logging()
 
-thing_server = ThingServer()
-thing_server.add_thing(StreamingPiCamera2(), "/camera/")
-thing_server.add_thing(SangaboardThing(), "/stage/")
-thing_server.add_thing(RecentringThing(), "/auto_recentre_stage/")
-thing_server.add_thing(AutofocusThing(), "/autofocus/")
-thing_server.add_thing(CameraStageMapper(), "/camera_stage_mapping/")
-thing_server.add_thing(SystemControlThing(), "/system_control/")
-thing_server.add_thing(SettingsManager(), "/settings/")
-thing_server.add_thing(SmartScanThing("application/openflexure-stitching/.venv/bin/openflexure-stitch"), "/smart_scan/")
-thing_server.add_thing(BackgroundDetectThing(), "/background_detect/")
-thing_server.add_thing(APITestThing(), "/api_test/")
-try:
-    add_static_files(thing_server.app)
-except RuntimeError:
-    print("Failed to add static files - you will have to do without them!")
+def customise_server(server: ThingServer):
+    """Customise the server with additional endpoints, etc."""
+    configure_logging()
+    add_v2_endpoints(server)
+    try:
+        add_static_files(server.app)
+    except RuntimeError:
+        print("Failed to add static files - you will have to do without them!")
 
-add_v2_endpoints(thing_server)  # Add the v2 endpoints for compatibility with OpenFlexure Connect
-
-# Add an endpoint to get the log
-thing_server.app.get("/log/")(retrieve_log)
+    # Add an endpoint to get the log
+    server.app.get("/log/")(retrieve_log)
 
 
-app = thing_server.app
+def serve_from_cli(argv: Optional[list[str]] = None):
+    """Start the server from the command line"""
+    args = cli.parse_args(argv)
+    try:
+        config, server = None, None
+        config = cli.config_from_args(args)
+        server = cli.server_from_config(config)
+        customise_server(server)
+        uvicorn.run(server.app, host=args.host, port=args.port)
+    except BaseException as e:
+        if args.fallback:
+            print(f"Error: {e}")
+            fallback_server = "labthings_fastapi.server.fallback:app"
+            print(f"Starting fallback server {fallback_server}.")
+            app = cli.object_reference_to_object(fallback_server)
+            app.labthings_config = config
+            app.labthings_server = server
+            app.labthings_error = e
+            uvicorn.run(app, host=args.host, port=args.port)
+        else:
+            raise e
+
 
