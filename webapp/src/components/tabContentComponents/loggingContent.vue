4f8e508dd580701679cc343e346aad5b90b29353
Joe Knapper <joe.knapper@glasgow.ac.uk>
2025-05-14 15:12:17 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Julian Stirling <julian@julianstirling.co.uk>

--- webapp/src/components/tabContentComponents/loggingContent.vue
+++ webapp/src/components/tabContentComponents/loggingContent.vue
@@ -153,7 +153,7 @@ export default {
       let response = await axios.get(this.logFileURI);
       let lines = response.data.split("\n");
       let logs = [];
-      let regexp = /\[(.+?)\] \[(.+?)\] (.*)$/;
+^\[(.+?)\] \[(DEBUG|INFO|WARNING|ERROR|CRITICAL)\] (.*)$
       for (let line of lines) {
         if (line.length > 0) {
           let m = line.match(regexp);
