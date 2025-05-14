a1ef332e313e820a288aa11faeb670ed9a3d80a3
jaknapper <jaknapper@hotmail.com>
2025-05-14 16:36:16 +0100

    Set regexp equal to new format

--- webapp/src/components/tabContentComponents/loggingContent.vue
+++ webapp/src/components/tabContentComponents/loggingContent.vue
@@ -153,7 +153,7 @@ export default {
       let response = await axios.get(this.logFileURI);
       let lines = response.data.split("\n");
       let logs = [];
-^\[(.+?)\] \[(DEBUG|INFO|WARNING|ERROR|CRITICAL)\] (.*)$
+      let regexp = /^\[(.+?)\] \[(DEBUG|INFO|WARNING|ERROR|CRITICAL)\] (.*)$/;
       for (let line of lines) {
         if (line.length > 0) {
           let m = line.match(regexp);
