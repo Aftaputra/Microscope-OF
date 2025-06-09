7fdb5f37ba12e93fc627dda713ebad75ddec4831
Julian Stirling <julian@julianstirling.co.uk>
2025-06-09 12:08:25 +0000

    Apply 4 suggestion(s) to 3 file(s)
    Co-authored-by: Beth Probert <beth_probert@outlook.com>

--- check_version_sync.py
+++ check_version_sync.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 """
-A script to check that th npm version string matches the python
+A script to check that the npm version string matches the python
 version string exactly.
 """
 
