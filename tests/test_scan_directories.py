f3a873ae5857daf7636cdb83bc8951616223c251
Beth Probert <beth_probert@outlook.com>
2025-07-02 09:51:27 +0000

    Apply suggestions from code review of branch download-stitched-image
    Co-authored-by: Joe Knapper <joe.knapper@glasgow.ac.uk>

--- tests/test_scan_directories.py
+++ tests/test_scan_directories.py
@@ -357,6 +357,7 @@ def test_all_files():
 
     # As standard for 8 layers there are 89 jpegs and 1 dzi file.
     assert len(set(all_files)) == 90
+    assert len(all_files) == 90
     dzi_file = None
     # Check all files exist
     for file in all_files:
