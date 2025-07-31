c00c027ab16fb2035849be17fbf5b11d030d6511
Joe Knapper <joe.knapper@glasgow.ac.uk>
2025-07-31 14:52:21 +0000

    Apply suggestions from code review of branch img-name
    Co-authored-by: Julian Stirling <julian@julianstirling.co.uk>

--- src/openflexure_microscope_server/things/autofocus.py
+++ src/openflexure_microscope_server/things/autofocus.py
@@ -141,7 +141,11 @@ class CaptureInfo:
 
     @property
     def filename(self) -> str:
-        """The filename for this image generated from the position, prepended with "img_"."""
+        """The filename for this image generated from the position.
+
+        The file name is in the format ``img_{x}_{y}_{z}`` where x, y, and z are the
+        positions from the microscope stage.
+        """
         return (
             f"img_{self.position['x']}_{self.position['y']}_{self.position['z']}.jpeg"
         )
