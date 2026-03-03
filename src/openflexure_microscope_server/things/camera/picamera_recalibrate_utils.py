84f30320226226a365f0672756c914a75a24b019
Joe Knapper <jaknapper@hotmail.com>
2026-03-03 14:52:29 +0000

    Update docstrings to explain lower brightness

--- src/openflexure_microscope_server/things/camera/picamera_recalibrate_utils.py
+++ src/openflexure_microscope_server/things/camera/picamera_recalibrate_utils.py
@@ -120,10 +120,11 @@ def adjust_shutter_and_gain_from_raw(
     :param camera: A Picamera2 object.
     :param target_white_level: The raw value we aim for, the raw value of the brightest
         pixels should be approximately this bright. The value to set depends on the
-        sensor bit depth. We recommend values of 700 for 10-bit sensors and 2800 for
-        12-bit sensors. This is about 70% of saturated once the blacklevel is
+        sensor bit depth. We recommend values of 400 for 10-bit sensors and 1600 for
+        12-bit sensors. This is about 40% of saturated once the blacklevel is
         subtracted. The maximum possible value depends on the sensor bit depth, the
-        sensor blacklevel and the tolerance argument.
+        sensor blacklevel and the tolerance argument. While this only uses 40% of the
+        sensor range, after gamma this corresponds to pixel value ~200.
     :param max_iterations: We will terminate once we perform this many iterations,
         whether or not we converge.  More than 10 shouldn't happen.
     :param tolerance: How close to the target value we consider "done".  Expressed as a
