713df248ffc1eb8d2d5df0255488ff17895f3577
Beth Probert <beth.probert@strath.ac.uk>
2026-03-06 09:53:41 +0000

    Add underscores to fix unit test

--- tests/unit_tests/test_utilities.py
+++ tests/unit_tests/test_utilities.py
@@ -48,7 +48,7 @@ def test_make_name_safe_reserved_names_false_positives():
     assert make_name_safe("CONSTANT") == "CONSTANT"
     assert make_name_safe("CON2") == "CON2"
     assert make_name_safe("ICON") == "ICON"
-    assert make_name_safe("CHILLI CON CARNE") == "CHILLI CON CARNE"
+    assert make_name_safe("CHILLI CON CARNE") == "CHILLI_CON_CARNE"
 
 
 def test_make_path_safe_basic():
