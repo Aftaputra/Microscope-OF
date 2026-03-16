d693cb9bdd56f6b812b3efd5a0325efd154c43a6
Joe Knapper <jaknapper@hotmail.com>
2026-03-16 16:47:48 +0000

    Added explicit wording for having no coverslip

--- webapp/src/components/modalComponents/calibrationWizardComponents/cameraCalibrationSteps/camCalibrationExplanation.vue
+++ webapp/src/components/modalComponents/calibrationWizardComponents/cameraCalibrationSteps/camCalibrationExplanation.vue
@@ -5,8 +5,8 @@
     </p>
     <ul class="uk-list uk-list-bullet">
       <li>Insert a slide.</li>
-      <li>Move your slide to an empty area, ideally looking through the coverslip.</li>
-      <li>If your slide has no empty areas, remove it and instead calibrate on no sample.</li>
+      <li>Move your slide to an empty area, ideally looking through the coverslip if your slide has one.</li>
+      <li>If your slide has no empty areas, remove it and instead calibrate with no sample.</li>
       <li>Ensure your illumination is on, well focused, and centred.</li>
     </ul>
   </div>
