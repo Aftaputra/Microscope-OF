d7872186c53d8de9c0dc030de3ade23c723a3c60
Joe Knapper <joe.knapper@glasgow.ac.uk>
2026-03-17 13:38:17 +0000

    Apply suggestions from code review of branch empty-cal
    Co-authored-by: Beth Probert <beth_probert@outlook.com>

--- webapp/src/components/modalComponents/calibrationWizardComponents/cameraCalibrationSteps/camCalibrationExplanation.vue
+++ webapp/src/components/modalComponents/calibrationWizardComponents/cameraCalibrationSteps/camCalibrationExplanation.vue
@@ -6,7 +6,7 @@
       </p>
       <ul class="uk-list uk-list-bullet">
         <li>Ensure your illumination is on, well focused, and centred.</li>
-        <li>Insert a slide with some empty regions.</li>
+        <li>Insert a slide with some empty areas.</li>
         <li>If your slide has no empty areas, remove it and instead calibrate with no sample.</li>
       </ul>
     </div>
