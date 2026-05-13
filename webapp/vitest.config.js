80b32800f928840b9cae6cce3103d54f22460115
Antonio Anaya <antonio.anaya@kny5.work>
2026-05-12 23:19:26 -0600

    ui_migration feature(unitesting) Add unitesting configuration and dependencies

---
+++ webapp/vitest.config.js
@@ -0,0 +1,23 @@
+
+// File: vitest.config.js
+// Generic configuration for vitest
+
+import { defineConfig } from 'vitest/config';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [vue()],
+
+  test: {
+    environment: 'jsdom',
+
+    globals: true,
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
