c1915759b4f407f705ca10a16498f6a6fba065ba
Julian Stirling <julian@julianstirling.co.uk>
2026-06-02 17:22:38 +0100

    Update unit tests for capture mode.

--- src/openflexure_microscope_server/things/__init__.py
+++ src/openflexure_microscope_server/things/__init__.py
@@ -82,6 +82,7 @@ class OFMThing(lt.Thing):
         return rel_data_path
 
 
+# TODO Write tests for this
 class RelativeDataPath(RootModel[str]):
     """A relative path that is validated, and can have a Thing assigned to it.
 
