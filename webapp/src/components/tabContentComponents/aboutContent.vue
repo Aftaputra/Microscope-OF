2bc5a95f25bf98c73a0651c5a6ac8883b2139c94
Joe Knapper <jaknapper@hotmail.com>
2026-06-12 16:12:52 +0100

    Links as bullet points

--- webapp/src/components/tabContentComponents/aboutContent.vue
+++ webapp/src/components/tabContentComponents/aboutContent.vue
@@ -6,19 +6,27 @@
     </div>
     <div class="uk-padding-small">
       <h2>Links</h2>
-      <a class="uk-link" target="_blank" href="https://openflexure.discourse.group/">
-        Help and community support - OpenFlexure Forum
-      </a>
-      <br />
-      <a
-        class="uk-link"
-        target="_blank"
-        href="https://gitlab.com/openflexure/openflexure-microscope-server/-/issues"
-      >
-        Report an issue
-      </a>
-      <br />
-      <a class="uk-link" target="_blank" href="https://openflexure.org"> OpenFlexure Homepage </a>
+      <ul>
+        <li>
+          <a class="uk-link" target="_blank" href="https://openflexure.discourse.group/">
+            Help and community support - OpenFlexure Forum
+          </a>
+        </li>
+        <li>
+          <a
+            class="uk-link"
+            target="_blank"
+            href="https://gitlab.com/openflexure/openflexure-microscope-server/-/issues"
+          >
+            Report an issue
+          </a>
+        </li>
+        <li>
+          <a class="uk-link" target="_blank" href="https://openflexure.org">
+            OpenFlexure Homepage
+          </a>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
