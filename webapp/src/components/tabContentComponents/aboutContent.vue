69faff982827702c4bc6fa040d6a10ed507cde89
Joe Knapper <jaknapper@hotmail.com>
2026-06-12 12:34:18 +0100

    Draft wording for an intended use disclaimer

--- webapp/src/components/tabContentComponents/aboutContent.vue
+++ webapp/src/components/tabContentComponents/aboutContent.vue
@@ -5,6 +5,16 @@
       <statusPane class="uk-width-large" />
     </div>
     <div class="uk-padding-small">
+      <h2>Intended use</h2>
+      <p>
+        The OpenFlexure system, including hardware, software and peripherals, is intended solely for
+        research, educational, training, and non-clinical use.<br />It is not a certified medical
+        device, and must not be relied upon for diagnosis, treatment, patient care, or other
+        clinical purposes. <br /><br />Any use outside the stated intended purpose is the sole
+        responsibility of the user.
+      </p>
+    </div>
+    <div class="uk-padding-small">
       <h2>Links</h2>
       <a
         class="uk-link"
